9df323057a7722a447ae90f1c8fc32636db94898
HieuTV <mphobao@gmail.com>
2023-07-25 11:40:45 +0700

    23

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -59,7 +59,7 @@ export class LoginComponent implements OnInit, AfterViewInit, OnDestroy {
     this.server = this.authService.serverModel.modelName;
     this.randomImageServiceSubscription = this.randomImageService.get().subscribe(x => this.backgroundImage = x.src);
 
- 
+    localStorage.clear();
     
     // this.authServiceLogoutSubscription = this.authService.logout(false).subscribe(() => {
     //   this.authService.authenticated$.next(false);
