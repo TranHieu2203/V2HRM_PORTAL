9398801a1cea183bd7d7d6fc7cd4b9cf4fbc2e30
HieuTV <hieutv1@vmogroup.com>
2023-07-31 17:34:13 +0700

    init quy trình đánh giá thử việc, các component con, load componet theo config

--- src/app/business-modules/hr-process/hr-process.module.ts
+++ src/app/business-modules/hr-process/hr-process.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HrProcessRoutes } from './hr-process.routing';
-import { EmployeeReportModule } from './probationary-assessment/employee-report/employee-report.module';
+import { EmployeeReportModule } from './probationary-assessment/1-employee-report/employee-report.module';
 import { ProbationaryAssessmentComponent } from './probationary-assessment/probationary-assessment.component';
 import { ProfileInfoModule } from './profile-info/profile-info.module';
+import { AccordionModule } from '@syncfusion/ej2-angular-navigations';
+import { DirectManagerReviewModule } from './probationary-assessment/2-direct-manager-review/direct-manager-review.module';
+import { LineManagerReviewModule } from './probationary-assessment/3-line-manager-review/line-manager-review.module';
+import { HrReviewModule } from './probationary-assessment/4-hr-review/4-hr-review.module';
+import { BodReviewModule } from './probationary-assessment/5-bod-review/5-bod-review.module';
 
 @NgModule({
 
   imports: [
     CommonModule,
     HrProcessRoutes,
+    AccordionModule,
+    ProfileInfoModule,
+    //probationary-assessment
     EmployeeReportModule,
-    ProfileInfoModule
+    DirectManagerReviewModule,
+    LineManagerReviewModule,
+    HrReviewModule,
+    BodReviewModule
+
   ],
   declarations: [
     ProbationaryAssessmentComponent
--- src/app/business-modules/hr-process/probationary-assessment/probationary-assessment.component.ts
+++ src/app/business-modules/hr-process/probationary-assessment/probationary-assessment.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeReportComponent } from './employee-report/employee-report.component';
+import { Component, ComponentFactoryResolver, Inject, OnInit, QueryList, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
+import { EmployeeReportComponent } from './1-employee-report/employee-report.component';
+import { ProfileInfoComponent } from '../profile-info/profile-info.component';
+import { DirectManagerReviewComponent } from './2-direct-manager-review/direct-manager-review.component';
+import { LineManagerReviewComponent } from './3-line-manager-review/line-manager-review.component';
+import { HrReviewComponent } from './4-hr-review/4-hr-review.component';
+import { BodReviewComponent } from './5-bod-review/5-bod-review.component';
+
+
+
 @Component({
   selector: 'app-probationary-assessment',
   templateUrl: './probationary-assessment.component.html',
   styleUrls: ['./probationary-assessment.component.css']
 })
 export class ProbationaryAssessmentComponent implements OnInit {
+  @ViewChild('embeddedContainer', { read: ViewContainerRef }) embeddedContainer!: ViewContainerRef;
 
-  constructor() { }
+  public selectedDosing = {
+    steps: [
+
+      {
+        label: '1-employee-report',
+        component: EmployeeReportComponent,
+      },
+      {
+        label: '2-direct-manager-review',
+        component: DirectManagerReviewComponent,
+      },
+      {
+        label: '3-line-manager-review',
+        component: LineManagerReviewComponent,
+      },
+      {
+        label: '4-hr-review',
+        component: HrReviewComponent,
+      },
+      {
+        label: '5-bod-review',
+        component: BodReviewComponent,
+      }
+    ]
+  }
+
+  constructor(
+  ) { }
 
   ngOnInit() {
+
+
   }
+
 
 }
